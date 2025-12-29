bf3d61006719e83cabd87196c66d959a8527cbaa
Ess <127221085+EstherLavender@users.noreply.github.com>
2025-12-30 00:24:52 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,19 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+// src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+import Providers from "@/components/Providers";
+
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Providers>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Providers>
+  </React.StrictMode>
+);
