ed65b5f2d232d51275702b65e44f10b7b112ff90
Ess <127221085+EstherLavender@users.noreply.github.com>
2025-12-30 00:24:09 +0300

    Update Providers.tsx

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,1 +1,25 @@
+// src/components/Providers.tsx
+import "@rainbow-me/rainbowkit/styles.css";
 
+import React from "react";
+import { WagmiProvider } from "wagmi";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { wagmiConfig } from "@/lib/wagmiConfig";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry: 1, refetchOnWindowFocus: false },
+  },
+});
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
